bc59fe79f8e9c51c48cf79484470984b77eb45bb
samwitry <54mw1tr7@gmail.com>
2017-04-14 11:28:16 +0200

    reworked text output nearly finished in hard code (will start coloring with bootstrap now)

--- app/Views/output/output.php
+++ app/Views/output/output.php
@@ -69,28 +69,112 @@
 
 <h2>output text</h2>
 
-<table class='table'>
+<table class='table'
+       style="border: 1px solid black;
+       /*border-collapse: collapse;*/
+       padding: 25px;">
     <thead>
-        <tr>
-            <th>Values NVA</th>
+        <tr<!--A-->>
+            <th>english<!--1--></th>
+            <th>francais<!--2--></th>
+            <th>type<!--3--></th>
+            <th>value<!--4--></th>
+            <th style="width: 25px;"><!--empty--><!--5--></th>
+            <th>Detail NVA<!--6--></th>
+            <th>Wastage<!--7--></th>
         </tr>
     </thead>
+
     <tbody>
-        <tr>
-            <td>sum VA</td>
-            <td>60</td>
+        <tr<!--B-->>
+            <td>added value<!--1--></td>            
+            <td>valeur ajoutée<!--2--></td>
+            <td>VA<!--3--></td>
+            <td>60<!--4--></td>
+            <td><!--5--></td>
+            <td><!--6--></td>
+            <td><!--7--></td>
         </tr>
-        <tr>
-            <td>sum NVA</td>
-            <td>40</td>
-        </tr>                
-        <tr>
-            <td>sum NVA</td>
-            <td>15</td>
+        <tr<!--C-->>
+            <td>NO added value<!--1--></td>            
+            <td>non valeur ajoutée totale<!--2--></td>
+            <td>NVA<!--3--></td>
+            <td>40<!--4--></td>
+            <td><!--5--></td>
+            <td>=>100<!--6--></td>
+            <td>% total de la VA et NVA,et la NVA est composé de <!--7--></td>
         </tr>
-        <tr>
-            <td>sum NVA-U</td>
-            <td>25</td>
-        </tr>         
+        <tr<!--C-->>
+            <td>usefull no added value<!--1--></td>            
+            <td>non valeur ajoutée utile<!--2--></td>
+            <td>NVA-u<!--3--></td>
+            <td>15<!--4--></td>
+            <td><!--5--></td>
+            <td>12<!--6--></td>
+            <td>Stocks Inutiles<!--7--></td>
+        </tr>
+        <tr<!--D-->>
+            <td>unusefull no added value<!--1--></td>            
+            <td>non valeur ajoutée inutile<!--2--></td>
+            <td>NVA-i<!--3--></td>
+            <td>25<!--4--></td>
+            <td><!--5--></td>
+            <td>2<!--6--></td>
+            <td>Surproduction<!--7--></td>
+        </tr>
+        <tr<!--F-->>
+            <td><!--1--></td>            
+            <td><!--2--></td>
+            <td><!--3--></td>
+            <td><!--4--></td>
+            <td><!--5--></td>
+            <td>32<!--6--></td>
+            <td>Déplacements<!--7--></td>
+        </tr>
+        <tr<!--G-->>
+            <td><!--1--></td>            
+            <td><!--2--></td>
+            <td><!--3--></td>
+            <td><!--4--></td>
+            <td><!--5--></td>
+            <td>5<!--6--></td>
+            <td>Surprocessing ou setup inutiles<!--7--></td>
+        </tr>
+        <tr<!--H-->>
+            <td><!--1--></td>            
+            <td><!--2--></td>
+            <td><!--3--></td>
+            <td><!--4--></td>
+            <td><!--5--></td>
+            <td>11<!--6--></td>
+            <td>Mouvements Inutiles<!--7--></td>
+        </tr>
+        <tr<!--I>-->>
+            <td><!--1--></td>            
+            <td><!--2--></td>
+            <td><!--3--></td>
+            <td><!--4--></td>
+            <td><!--5--></td>
+            <td>13<!--6--></td>
+            <td>Retravail<!--7--></td>
+        </tr>
+        <tr<!--J-->>
+            <td><!--1--></td>            
+            <td><!--2--></td>
+            <td><!--3--></td>
+            <td><!--4--></td>
+            <td><!--5--></td>
+            <td>25<!--6--></td>
+            <td>Sous-utilisation des Compétences<!--7--></td>
+        </tr>
+        <tr<!--K-->>
+            <td><!--1--></td>            
+            <td><!--2--></td>
+            <td><!--3--></td>
+            <td><!--4--></td>
+            <td><!--5--></td>
+            <td><!--6--></td>
+            <td><!--7--></td>
+        </tr>
     </tbody>
 </table>
