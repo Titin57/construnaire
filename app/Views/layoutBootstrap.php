6f84d9467a1af1ad942ad6e6d6d384c2391898d5
CarloSpecchio <carlo.specchio@vo.lu>
2017-04-14 12:39:04 +0200

    corrections on menu

--- app/Views/layoutBootstrap.php
+++ app/Views/layoutBootstrap.php
@@ -11,7 +11,7 @@
 
         <!-- Optional theme -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-        
+
         <!-- jQuery -->
         <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
 
@@ -29,23 +29,31 @@
                         <!-- Collect the nav links, forms, and other content for toggling -->
                         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                             <ul class="nav navbar-nav">
-                                <li<?php if ($currentPage == 'est'): ?> class="active"<?php endif; ?>>
-                                    <a href="<?= $this->url('conference_est') ?>">Conférence Est</a>
+                                <li<?php if ($currentPage == 'home'): ?> class="active"<?php endif; ?>>
+                                    <a href="<?= $this->url('default_home') ?>">Teams</a>
                                 </li>
-                                <li<?php if ($currentPage == 'ouest'): ?> class="active"<?php endif; ?>>
-                                    <a href="<?= $this->url('conference_ouest') ?>">Conférence Ouest</a>
+                                <li<?php if ($currentPage == 'home'): ?> class="active"<?php endif; ?>>
+                                    <a href="<?= $this->url('default_home') ?>">Tasks</a>
+                                </li>
+                                <li<?php if ($currentPage == 'home'): ?> class="active"<?php endif; ?>>
+                                    <a href="<?= $this->url('default_home') ?>">A/M Task</a>
                                 </li>
                                 <li<?php if ($currentPage == 'contact'): ?> class="active"<?php endif; ?>>
-                                    <a href="<?= $this->url('default_contact') ?>">Contact</a>
+                                    <a href="<?= $this->url('default_home') ?>">Process</a>
                                 </li>
+                                <li<?php if ($currentPage == 'output'): ?> class="active"<?php endif; ?>>
+                                    <a href="<?= $this->url('output_outputText') ?>">Summery</a>
+                                </li>
+                                <li <?php if ($currentPage == 'addWorker'): ?>class="active"<?php endif; ?> ><a href="<?= $this->url('worker_addWorker') ?>">Worker</a></li>
+                                <li role="separator" class="divider"></li>                                
                                 <?php if (empty($w_user)) : ?>
-                                <li<?php if ($currentPage == 'signin'): ?> class="active"<?php endif; ?>>
-                                    <a href="<?= $this->url('user_signin') ?>">Connexion</a>
-                                </li>
+                                    <li<?php if ($currentPage == 'signin'): ?> class="active"<?php endif; ?>>
+                                        <a href="<?= $this->url('user_signin') ?>">Connexion</a>
+                                    </li>
                                 <?php else : ?>
-                                <li>
-                                    <a href="<?= $this->url('user_logout') ?>">Déconnexion</a>
-                                </li>
+                                    <li>
+                                        <a href="<?= $this->url('user_logout') ?>">Déconnexion</a>
+                                    </li>
                                 <?php endif; ?>
                             </ul>
                         </div><!-- /.navbar-collapse -->
@@ -53,11 +61,11 @@
                 </nav>
                 <h1><?= $this->e($title) ?></h1>
             </header>
-            
+
             <?php if (isset($w_flash_message) && !empty($w_flash_message->message)): ?>
-            <div class="alert alert-<?= $w_flash_message->level ?>" role="alert">
-                <?= $w_flash_message->message ?>
-            </div>
+                <div class="alert alert-<?= $w_flash_message->level ?>" role="alert">
+                    <?= $w_flash_message->message ?>
+                </div>
             <?php endif; ?>
 
             <section>
