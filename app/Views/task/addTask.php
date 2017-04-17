d9c764aac8558980f0b18a9eba0920ce5b38b363
DESKTOP-KOF9GBT\Seb <m_donovan@hotmail.fr>
2017-04-17 12:53:29 +0200

    Mise en place des curseurs pour les jauges VA / NVA / NVAU

--- app/Views/task/addTask.php
+++ app/Views/task/addTask.php
@@ -45,16 +45,19 @@
                     <td><textarea name="tas_penality" rows="5" cols="100"></textarea></td>
 		</tr>
                 <tr>
-                    <td>VA :&nbsp;</td>
-                    <td><textarea name="tas_va" rows="5" cols="100"></textarea></td>
+                    <td>VA :&nbsp;</td>                 
+                    <td><div class="slider" name="tas_va" ></div></td>
+                    <td><input type="text" class="val"></td>
 		</tr>
                 <tr>
                     <td>NVA :&nbsp;</td>
-                    <td><textarea name="tas_nva" rows="5" cols="100"></textarea></td>
+                    <td><div class="slider2" name="tas_nva" ></div></td>
+                    <td><input type="text" class="val2"></td>
 		</tr>
                 <tr>
                     <td>NVAU :&nbsp;</td>
-                    <td><textarea name="tas_nvau" rows="5" cols="100"></textarea></td>
+                    <td><div class="slider3" name="nvau" ></div></td>
+                    <td><input type="text" class="val3"></td>
 		</tr>
                 <tr>
                     <td>Start :&nbsp;</td>
@@ -81,7 +84,52 @@
   $( function() {
     $( "#datepicker" ).datepicker();
   } );
-  </script>
+</script>
+
+<script>
+  $( function() {
+    $( ".slider" ).slider({
+        min: 0,
+        max: 100,
+        step: 10,
+        value: 50,
+        slide: function( event, ui){
+            $(".val").val(ui.value );
+        }
+    });
+    $(".val").val($( ".slider" ).slider("value"));
+  });
+</script>
+
+<script>
+  $( function() {
+    $( ".slider2" ).slider({
+        min: 0,
+        max: 100,
+        step: 10,
+        value: 50,
+        slide: function( event, ui){
+            $(".val2").val(ui.value );
+        }
+    });
+    $(".val2").val($( ".slider2" ).slider("value"));
+  });
+</script>
+
+<script>
+  $( function() {
+    $( ".slider3" ).slider({
+        min: 0,
+        max: 100,
+        step: 10,
+        value: 50,
+        slide: function( event, ui){
+            $(".val3").val(ui.value );
+        }
+    });
+    $(".val3").val($( ".slider3" ).slider("value"));
+  });
+</script>
 
 
 
