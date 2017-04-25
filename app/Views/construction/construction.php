137b5a362c59182fbb7de6fe877d8d36efa7a85f
DESKTOP-KOF9GBT\Seb <m_donovan@hotmail.fr>
2017-04-25 22:32:54 +0200

    Vive le style !

--- app/Views/construction/construction.php
+++ app/Views/construction/construction.php
@@ -2,23 +2,25 @@
 
 <?php $this->start('main_content') ?>
 <h3>Construction</h3>
-<table class="table">
-    <thead>
-        <tr>
-            <th>#</th>  
-            <th>Construction</th>   
-            <th>Start date</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($allConstruction as $currentConstruction) : ?>
+<div class="panel panel-primary">
+    <table class="table">
+        <thead>
             <tr>
-                <td><?= $currentConstruction['con_id'] ?></td>
-                <!--- link to the current item returned by the id -->
-                <td><a href="<?= $this->url('construction_listconstruction', array('con_id' => $currentConstruction['con_id'], 'conName' => $currentConstruction['con_name'])); ?>"><?= $currentConstruction['con_name'] ?></a></td>
-                <td><?= $currentConstruction['con_startdate'] ?></td>
+                <th>#</th>  
+                <th>Construction</th>   
+                <th>Start date</th>
             </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            <?php foreach ($allConstruction as $currentConstruction) : ?>
+                <tr>
+                    <td><?= $currentConstruction['con_id'] ?></td>
+                    <!--- link to the current item returned by the id -->
+                    <td><a href="<?= $this->url('construction_listconstruction', array('con_id' => $currentConstruction['con_id'], 'conName' => $currentConstruction['con_name'])); ?>"><?= $currentConstruction['con_name'] ?></a></td>
+                    <td><?= $currentConstruction['con_startdate'] ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
 <?php $this->stop('main_content') ?>
--- app/Views/construction/listconstruction.php
+++ app/Views/construction/listconstruction.php
@@ -2,34 +2,36 @@
 
 <?php $this->start('main_content') ?>
 <h3>All constructions</h3>
-<table class="table">
-    <thead>
-        <tr>
-            <th>Construction</th>   
-            <th>Date started</th>
-        </tr>
-    </thead>
-    <tbody>
-
-        <?php foreach ($allConstruction as $currentConstruction) : ?>
+<div class="panel panel-primary">
+    <table class="table">
+        <thead>
             <tr>
-                <!--- 
-                in order to make an hyperlink from this to a other page's
-                the link to the page must be added as php here 
-                -->
-                <!--- in this part the link to the tasks overview is defined
-                to do next: link to the task(s) related to the construction -->
-                <td><a href="<?= $this->url('tasks_viewtasks'); ?>"><?= $currentConstruction['con_name'] ?></a></td>
-                <td><?= $currentConstruction['con_startdate'] ?></td>
+                <th>Construction</th>   
+                <th>Date started</th>
             </tr>
-        <?php endforeach; ?>
-    <!--- code for buttons to display page addconstruction -->
-    <ul class="nav nav-pills">
-        <li role="presentation" class=""><a href="add/">Add new construction</a></li>
+        </thead>
+        <tbody>
 
-    </ul>
+            <?php foreach ($allConstruction as $currentConstruction) : ?>
+                <tr>
+                    <!--- 
+                    in order to make an hyperlink from this to a other page's
+                    the link to the page must be added as php here 
+                    -->
+                    <!--- in this part the link to the tasks overview is defined
+                    to do next: link to the task(s) related to the construction -->
+                    <td><a href="<?= $this->url('tasks_viewtasks'); ?>"><?= $currentConstruction['con_name'] ?></a></td>
+                    <td><?= $currentConstruction['con_startdate'] ?></td>
+                </tr>
+            <?php endforeach; ?>
+        <!--- code for buttons to display page addconstruction -->
+        <ul class="nav nav-pills">
+            <li role="presentation" class=""><a href="add/">Add new construction</a></li>
 
-</tbody>
-</table>
+        </ul>
+
+    </tbody>
+    </table>
+</div>
 
 <?php $this->stop('main_content') ?>
